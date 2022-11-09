82cd165b756a352dfff049fa9eef119f66b5a029
Waiz Khan <68347659+Waiz-Legion@users.noreply.github.com>
2022-11-09 14:58:20 +0530

    Update deployement.py

--- deployement.py
+++ deployement.py
@@ -5,9 +5,7 @@ def predict(artifact_list, payload):
     payload_dict = json.loads(payload)
     model = artifact_list[0]
 
-    feature_names = ['2', '3', '4', '16', '13', '12', 
-        '35', '9', '18', '25', '8', '19', '7', '15', '22', 
-        '33', '14', '10', '5', '64', '29', '11', '17', '70', '36']
+    feature_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
 
     prediction_list = [payload_dict[feature] for feature in feature_names]
     prediction_vector = np.array(prediction_list).reshape(1, -1)
