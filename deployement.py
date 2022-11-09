2dfce3b5bd17e10fb79148c54e1d55a78523efb8
Waiz Khan <68347659+Waiz-Legion@users.noreply.github.com>
2022-11-09 15:34:18 +0530

    Update deployement.py

--- deployement.py
+++ deployement.py
@@ -5,8 +5,10 @@ def predict(artifact_list, payload):
     payload_dict = json.loads(payload)
     model = artifact_list[0]
 
-    feature_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-
+    baseline = artifact_list[2]
+    feature_names = []
+    for name, dict in baseline.items():
+        feature_names.append(name)
     prediction_list = [payload_dict[feature] for feature in feature_names]
     prediction_vector = np.array(prediction_list).reshape(1, -1)
 
